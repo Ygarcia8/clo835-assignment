103e48ce0bab423c95910f8bbb93038513de7de1
Ygarcia8 <yasming1608@gmail.com>
2025-09-28 23:00:10 +0000

    update app message

--- app/app.py
+++ app/app.py
@@ -1,24 +1,11 @@
-nano app/Dockerfile
-nano app/requirements.txt
-nano terraform/main.tf
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
-COLOR = os.environ.get("APP_COLOR", "blue")
-
-@app.route("/")
+@app.route('/')
 def home():
-    return f"""
-    <html>
-        <head><title>CLO835 Assignment</title></head>
-        <body style='background-color:{COLOR};'>
-            <h1>Hello from CLO835-Yasmin App!</h1>
-            <p>This application is running inside Docker with background color: <b>{COLOR}</b></p>
-        </body>
-    </html>
-    """
+    return "Hello from CLO835 WebApp running in Docker app!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
